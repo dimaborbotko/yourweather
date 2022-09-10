8949a5d905f36994e17911c59d56c7725c8fd016
dd <dimashechka0213@gmail.com>
2022-09-10 16:31:07 +0300

    #3 configured env file; do test request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { URL_WEATHER_API, WEAHTER_API } from "./api";
+
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        `${URL_WEATHER_API}/weather?lat=44.34&lon=10.99&appid=${WEAHTER_API}`
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
   return (
     <div>
       <p>Hello!</p>
---
+++ src/api.js
@@ -0,0 +1,2 @@
+export const URL_WEATHER_API = `${process.env.REACT_APP_URL_WEATHER_API}`
+export const WEAHTER_API = `${process.env.REACT_APP_WEATHER_API}`
